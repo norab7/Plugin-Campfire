7558577a02cf049c00b58998f5c617fa7574563f
norab7 <stuart.baron@gmail.com>
2018-12-20 03:07:01 +0000

    basefiles and yaml

--- src/baron/rol/main/TestClass.java
+++ src/baron/rol/main/TestClass.java
@@ -1,5 +1,17 @@
 package baron.rol.main;
 
-public class TestClass {
+import org.bukkit.plugin.java.JavaPlugin;
 
+public class TestClass extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+       
+    }
+   
+    @Override
+    public void onDisable() {
+       
+    }
+	
 }
